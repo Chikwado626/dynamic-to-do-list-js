ec8debfbc532bc82a0b75bdbea7058d750c95fb1
Chikwado626 <chikwadoezemma03@gmail.com>
2024-07-19 21:53:40 +0100

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
-
+    const element = document.getElementById("myElement");
+    element.classList.add("newClass");
     // Load tasks from Local Storage
     loadTasks();
 
